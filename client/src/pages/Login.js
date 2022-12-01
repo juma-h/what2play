8bed5a9dbb6e7f609719798c1a4330c144b29361
Juma-h <mitchfibi@gmail.com>
2022-12-01 17:10:03 +0300

    added resources

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,7 @@
 import styled from 'styled-components/macro';
+import landing from "../images/landing.png";
+import "../styles/login.css";
+
 
 const StyledLoginContainer = styled.main`
   display: flex;
@@ -6,6 +9,7 @@ const StyledLoginContainer = styled.main`
   justify-content: space-around;
   align-items: center;
   height: 100vh;
+  width:100%;
 `;
 
 const StyledLoginButton = styled.a`
@@ -30,11 +34,13 @@ const StyledLandingDiv = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
+  width:50%;
 `;
 
 const Login = () => (
   <StyledLoginContainer>
     <StyledLandingDiv>
+      <img classname="landing-img" src={landing} alt=""/>
       <h2>Choose | Prompt |Play </h2>
 
     </StyledLandingDiv>
